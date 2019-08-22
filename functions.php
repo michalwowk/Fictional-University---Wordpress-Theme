a0f90be2c2add669026bd0b391a3eeb83dbb473d
unknown <michalwowk92@gmail.com>
2019-08-22 13:22:38 +0200

    Created Programs Post Type - related with event in both ways

--- functions.php
+++ functions.php
@@ -36,6 +36,12 @@ function university_adjust_queries($query) {
             )
         ));
     }
+
+    if (!is_admin() && is_post_type_archive('program') && $query->is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('post_per_page', '-1');
+    }
 }
 
 add_action( 'pre_get_posts', 'university_adjust_queries' );
